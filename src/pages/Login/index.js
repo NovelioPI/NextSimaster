19d024f5b49e5c9e0e0265c29063dc7b42f7ca97
NovelioPI <novelio.p.i@mail.ugm.ac.id>
2022-09-26 13:42:38 +0700

    Creat style sheet & method for login view

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { View, Image, StyleSheet, Dimensions } from "react-native"
+import { View, Image, StyleSheet, Dimensions, Text } from "react-native"
 import background from '../../assets/images/login-bg.png'
 import simaster from '../../assets/images/logo-simaster.png'
 
@@ -7,31 +7,64 @@ export default class Login extends Component {
 	render() {
 		return (
 			<View style={this.styles.container}>
+				<this.loginView />
+				<this.loginUser />
+			</View>
+		)
+	}
+
+	loginView = () => {
+		return (
+			<View style={this.styles.overlay}>
 				<View style={this.styles.backgroundContainer}>
 					<Image source={background} />
 				</View>
-				<View>
+				<View style={this.styles.logoContainer}>
 					<Image source={simaster} style={this.styles.logo} />
+				</View>
+				<View style={this.styles.watermarkContainer}>
+					<Text>2022. Simaster Reserved by UGM</Text>
 				</View>
 			</View>
 		)
 	}
 
+	loginUser = () => {
+		return (
+			<View style={this.styles.overlay}>
+				
+			</View>
+		)
+	}
+
 	styles = StyleSheet.create({
-		backgroundContainer: {
-			position: 'absolute',
+		container: {
 			flex: 1,
 		},
-		container: {
-			backgroundColor: '#ffffff',
-			flex: 1,
-			alignItems: 'center',
+		backgroundContainer: {
+			position: 'absolute',
+		},
+		logoContainer: {
+			alignItems:'center',
 		},
 		logo: {
 			width: Dimensions.get('screen').width*0.7,
-			height: Dimensions.get('screen').width*0.7,
+			height: undefined,
+			aspectRatio: 1,
 			marginTop: Dimensions.get('screen').height*0.08,
-			alignItems: 'center',
+		},
+		watermarkContainer: {
+			flex:1,
+			justifyContent:'flex-end',
+			alignItems:'center',
+			bottom: 14,
+		},
+		overlay: {
+			position: 'absolute',
+			left: 0,
+			top: 0,
+			width: '100%',
+			height: '100%'
 		}
 	})
 }
