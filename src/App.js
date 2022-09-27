2075aaa1e336fdb093b1469c3b3b90073d3eddae
NovelioPI <novelio.p.i@mail.ugm.ac.id>
2022-09-27 18:10:32 +0700

    Add login input for user

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
-import React, { Component } from "react"
-import { Image, StyleSheet, Text, TextInput, View } from "react-native"
-import Login from './pages/Login'
+import React from 'react';
+import Login from './pages/Login';
 
 const App = () => {
-  return (
-    <Login />
-  )
-}
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#669ccf'
-  }
-})
+  return <Login />;
+};
 
 export default App;
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,70 +1,137 @@
-import React, { Component } from "react"
-import { View, Image, StyleSheet, Dimensions, Text } from "react-native"
-import background from '../../assets/images/login-bg.png'
-import simaster from '../../assets/images/logo-simaster.png'
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component, useState} from 'react';
+import {
+  View,
+  Image,
+  StyleSheet,
+  Dimensions,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+
+import background from '../../assets/images/login-bg.png';
+import simaster from '../../assets/images/logo-simaster.png';
 
 export default class Login extends Component {
-	render() {
-		return (
-			<View style={this.styles.container}>
-				<this.loginView />
-				<this.loginUser />
-			</View>
-		)
-	}
+  render() {
+    return (
+      <View style={this.styles.container}>
+        <this.loginView />
+        <this.loginUser />
+      </View>
+    );
+  }
 
-	loginView = () => {
-		return (
-			<View style={this.styles.overlay}>
-				<View style={this.styles.backgroundContainer}>
-					<Image source={background} />
-				</View>
-				<View style={this.styles.logoContainer}>
-					<Image source={simaster} style={this.styles.logo} />
-				</View>
-				<View style={this.styles.watermarkContainer}>
-					<Text>2022. Simaster Reserved by UGM</Text>
-				</View>
-			</View>
-		)
-	}
+  loginView = () => {
+    return (
+      <View style={this.styles.overlay}>
+        <View style={this.styles.backgroundContainer}>
+          <Image source={background} />
+        </View>
+        <View style={this.styles.logoContainer}>
+          <Image source={simaster} style={this.styles.logo} />
+        </View>
+        <View style={this.styles.watermarkContainer}>
+          <Text>2022. Simaster Reserved by UGM</Text>
+        </View>
+      </View>
+    );
+  };
 
-	loginUser = () => {
-		return (
-			<View style={this.styles.overlay}>
-				
-			</View>
-		)
-	}
+  loginUser = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-	styles = StyleSheet.create({
-		container: {
-			flex: 1,
-		},
-		backgroundContainer: {
-			position: 'absolute',
-		},
-		logoContainer: {
-			alignItems:'center',
-		},
-		logo: {
-			width: Dimensions.get('screen').width*0.7,
-			height: undefined,
-			aspectRatio: 1,
-			marginTop: Dimensions.get('screen').height*0.08,
-		},
-		watermarkContainer: {
-			flex:1,
-			justifyContent:'flex-end',
-			alignItems:'center',
-			bottom: 14,
-		},
-		overlay: {
-			position: 'absolute',
-			left: 0,
-			top: 0,
-			width: '100%',
-			height: '100%'
-		}
-	})
+    return (
+      <View style={this.styles.overlay}>
+        <View style={this.styles.inputContainer}>
+          <View style={this.styles.inputView}>
+            <TextInput
+              name="Username"
+              placeholder="Email UGM"
+              value={email}
+              onChangeText={text => setEmail(text)}
+            />
+          </View>
+
+          <View style={this.styles.inputView}>
+            <TextInput
+              name="password"
+              placeholder="Password"
+              autoCapitalize="none"
+              secureTextEntry={true}
+              value={password}
+              enablesReturnKeyAutomatically
+              onChangeText={text => setPassword(text)}
+            />
+          </View>
+
+          <TouchableOpacity style={this.styles.forgetButton}>
+            <Text>Forgot Password?</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={this.styles.loginButton}>
+            <Text style={{fontWeight: 'bold'}}>LOGIN</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
+  styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    backgroundContainer: {
+      position: 'absolute',
+    },
+    logoContainer: {
+      alignItems: 'center',
+    },
+    logo: {
+      width: Dimensions.get('screen').width * 0.7,
+      height: undefined,
+      aspectRatio: 1,
+      marginTop: Dimensions.get('screen').height * 0.08,
+    },
+    watermarkContainer: {
+      flex: 1,
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      bottom: 14,
+    },
+    overlay: {
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      width: '100%',
+      height: '100%',
+    },
+    inputView: {
+      borderBottomWidth: 1,
+      width: '70%',
+      height: 50,
+    },
+    inputContainer: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      marginBottom: '20%',
+    },
+    forgetButton: {
+      width: '70%',
+      marginTop: 5,
+      alignItems: 'flex-end',
+    },
+    loginButton: {
+      width: '70%',
+      borderRadius: 20,
+      backgroundColor: '#FFC82F',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 40,
+      marginTop: 40,
+    },
+  });
 }
